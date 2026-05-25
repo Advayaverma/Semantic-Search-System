edbeaf42cec9c7f5be909e79eaab4b69f632a0a9
Advayaverma <advaya2208@gmail.com>
2026-05-25 23:16:31 +0530

    ..

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import fetch_20newsgroups
 from sentence_transformers import SentenceTransformer
 import numpy as np
+# Save Embeddings to Disk (Faster Startup)
 
 # Load dataset
 def load_dataset():
@@ -15,7 +16,7 @@ def load_dataset():
     # Remove very short documents (noise)
     documents = [doc for doc in documents if len(doc) > 50]
 
-    return documents, dataset.target_names
+    return documents
 
 
 # Load embedding model
@@ -24,13 +25,13 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def embed_documents(documents):
 
-    embeddings = model.encode(documents, show_progress_bar=True, normalize_embeddings=True)
+    embeddings = model.encode(documents, show_progress_bar=True)
 
     return np.array(embeddings)
 
 
 def embed_query(query):
 
-    embedding = model.encode([query], normalize_embeddings=True)[0]
+    embedding = model.encode([query])[0]
 
     return embedding
